5fa4be58c9a9402071c8edfa9be3f76498279eb7
Jesus Rodriguez <bustasirio@gmail.com>
2021-09-07 18:44:16 -0600

    Secret renewed, shared prefs with token's info

--- app/src/main/java/com/bustasirio/spotifyapi/ui/view/activities/MainActivity.kt
+++ app/src/main/java/com/bustasirio/spotifyapi/ui/view/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bustasirio.spotifyapi.ui.view.activities
 
-import android.content.Intent
+import android.content.Context
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -11,17 +12,9 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import com.bustasirio.spotifyapi.R
-import com.bustasirio.spotifyapi.data.model.AuthorizationModel
-import com.bustasirio.spotifyapi.data.network.SpotifyAccountsClient
 import com.bustasirio.spotifyapi.databinding.ActivityMainBinding
 import com.bustasirio.spotifyapi.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import okhttp3.Response
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.net.URLEncoder
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -72,61 +65,30 @@ class MainActivity : AppCompatActivity() {
             Log.i("uri", "$uri")
 
             mainViewModel.code.value = uri.getQueryParameter("code")!!
+            mainViewModel.authorizationBasic.value = resources.getString(R.string.spotify_basic)
 
             mainViewModel.getAuth()
 
-            val response = mainViewModel.responseAuth.value
+            mainViewModel.response.observe(this, {
+                Log.d("tagMainActivityResponse", it.accessToken)
+                //TODO Save info on Shared prefs then pass to HomeActivity
+                val sharedPrefs = getPreferences(Context.MODE_PRIVATE)
+                with (sharedPrefs.edit()) {
+                    putString(getString(R.string.spotify_access_token), it.accessToken)
+                    putString(getString(R.string.spotify_token_type), it.tokenType)
+                    putString(getString(R.string.spotify_refresh_token), it.refreshToken)
+                    putBoolean(getString(R.string.spotify_logged), true)
+                    apply()
+                }
+            })
 
-            if( response != null ) {
-                Log.d("tagMainActivity", response.accessToken)
-            }
-
-//            val code : String = uri.getQueryParameter("code")!!
-//
-//            val authorizationString = "$clientId:${resources.getString(R.string.spotify_secret)}"
-//            val bytes = URLEncoder.encode(authorizationString, "UTF-8")
-//            Log.i("base64", bytes)
-////            val base64Str = authorizationString.encode()
-//            val base64Str = "XHgzMVx4MzVceDMxXHg2NVx4NjVceDYxXHgzNlx4NjRceDMyXHg2Mlx4MzdceDYxXHgzNFx4MzZceDMzXHgzMFx4NjFceDYzXHg2NVx4MzRceDMwXHgzMFx4MzBceDM1XHg2Nlx4MzBceDMxXHgzNFx4MzBceDM1XHg2Mlx4MzZceDNhXHg2MVx4NjJceDM0XHgzOFx4MzVceDY2XHg2NVx4MzlceDY1XHg2NFx4MzdceDYzXHgzNFx4MzRceDM5XHgzNVx4NjJceDY0XHgzN1x4NjNceDYzXHgzOVx4NjJceDY2XHg2Mlx4NjJceDM3XHgzOFx4NjNceDMwXHg2NFx4NjE="
-//
-//            Log.i("base64", base64Str)
-//            val authorizationBasic = "Basic $base64Str"
-//
-//            val builder : Retrofit.Builder = Retrofit.Builder()
-//                .baseUrl(baseUrl)
-//                .addConverterFactory(GsonConverterFactory.create())
-//
-//            val retrofit : Retrofit = builder.build()
-//
-//            val client : SpotifyAccountsClient = retrofit.create(SpotifyAccountsClient::class.java)
-//
-//            val accessTokenCall : Call<AuthorizationModel> = client.getAccessToken(
-//                authorizationBasic,
-//                "authorization_code",
-//                code,
-//                redirectUri
-//            )
-//
-//            accessTokenCall.enqueue(object : Callback<AuthorizationModel> {
-//                override fun onResponse(
-//                    call: Call<AuthorizationModel>,
-//                    response: Response<AuthorizationModel>
-//                ) {
-//                    val intent = Intent(this@MainActivity, HomeActivity::class.java)
-//                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//                    startActivity(intent)
-//                    finish()
-//                }
-//
-//                override fun onFailure(call: Call<AuthorizationModel>, t: Throwable?) {
-//                    Log.i("onFailure call ", "$t")
-//                    Toast.makeText(
-//                        this@MainActivity,
-//                        "Something wrong occurred",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//            })
+            mainViewModel.responseError.observe(this, {
+                if( it != null ){
+                    Toast.makeText(this, "Error: $it, try again later.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Error. Try again later.", Toast.LENGTH_SHORT).show()
+                }
+            })
 
 
         }
